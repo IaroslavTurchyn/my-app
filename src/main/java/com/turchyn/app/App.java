890a668f32fb12bb305f6c9f89d203b0c2c3a330
Yaroslav Turchyn <tur.chynip@gmail.com>
2022-10-13 22:19:55 +0300

    add initial application sources

---
+++ src/main/java/com/turchyn/app/App.java
@@ -0,0 +1,15 @@
+package com.turchyn.app;
+import com.turchyn.lib.Math;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Math math = new Math();
+        System.out.println( math.sum(4,2));
+    }
+}
